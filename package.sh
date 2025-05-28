4aefc881fb7b86470468f445462b9e42b379a4fa
Levy A. <levyddsa@gmail.com>
2025-05-28 09:05:30 -0300

    feat(ci): add cd release workflow

---
+++ package.sh
@@ -0,0 +1,33 @@
+
+set -xe
+
+target="${1:?"explit target is required"}"
+if [[ "$OSTYPE" == "msys" || "$OSTYPE" == "win32" ]]; then
+    manifest="$(powershell -Command "[System.IO.Path]::GetFullPath('$2')")"
+else
+    manifest="$(realpath ${2:?"explicit manifest path is required"})"
+fi
+
+for extra in '' --release; do
+  artifacts=$(
+    cargo build \
+      --message-format json \
+      --target $target \
+      --manifest-path $manifest \
+      $extra \
+      | jq -r --arg manifest "$manifest" '
+            select(.manifest_path == $manifest)
+            | .filenames[]
+        '
+  )
+
+  artifacts_dir="$(echo "$artifacts" | sed '1s|[/\\][^/\\]*$||;q')"
+  type="$(echo "$artifacts_dir" | sed 's|.*[/\\]||')"
+
+  mkdir -p dist/
+
+  echo "$artifacts" | zip -j@ dist/$target-$type.zip
+
+  zip -j dist/$target-$type.zip "$(dirname $manifest)/libsql.h"
+done
+
