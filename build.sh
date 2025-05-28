6501990e1213483aa4bb9ca4ef8a24e553ce7d75
Levy A. <levyddsa@gmail.com>
2025-05-28 09:05:30 -0300

    add CI releases

--- build.sh
+++ build.sh
@@ -4,11 +4,13 @@ set -xe
 
 RUSTFLAGS+="--remap-path-prefix=$HOME=~"
 
-cargo build --target x86_64-apple-darwin --features encryption --release
-cargo build --target aarch64-apple-darwin --features encryption --release
-cargo build --target aarch64-unknown-linux-gnu --features encryption --release
-cargo build --target x86_64-unknown-linux-gnu --features encryption --release
-cargo build --target x86_64-pc-windows-gnu --features encryption --release
+extra=${1:-}
 
-RUSTFLAGS+=" -C target-feature=-crt-static" cargo build --target aarch64-unknown-linux-musl --features encryption --release
-RUSTFLAGS+=" -C target-feature=-crt-static" cargo build --target x86_64-unknown-linux-musl --features encryption --release
+cargo build --target x86_64-apple-darwin --features encryption $extra
+cargo build --target aarch64-apple-darwin --features encryption $extra
+cargo build --target aarch64-unknown-linux-gnu --features encryption $extra
+cargo build --target x86_64-unknown-linux-gnu --features encryption $extra 
+cargo build --target x86_64-pc-windows-gnu --features encryption $extra
+
+RUSTFLAGS+=" -C target-feature=-crt-static" cargo build --target aarch64-unknown-linux-musl --features encryption $extra
+RUSTFLAGS+=" -C target-feature=-crt-static" cargo build --target x86_64-unknown-linux-musl --features encryption $extra
